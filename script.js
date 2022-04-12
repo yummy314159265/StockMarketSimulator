71569de96d37545bd9bbd622e4b1322292b9a127
KokeHaus <austinbcw@gmail.com>
2022-04-12 14:26:25 -0500

    add users

---
+++ script.js
@@ -0,0 +1,28 @@
+ Users=[
+     {
+         id:0,
+         name: "username",
+         password: "password123",
+     },
+     {
+         id:1,
+         name: "koki",
+         password:"password123",
+     }
+ ]
+
+//no duplicate usernames
+ function checkRepeat(){
+     if name.exists()
+     return null;
+ }
+
+//create account
+function storeUser(){
+
+}
+
+//login functionality
+function verifyUser(){
+
+}
