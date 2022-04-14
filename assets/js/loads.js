d23bd53325fb556c5277c09322cbccb58a015c38
Rodin Grajo <scaredmexicanlaborer@protonmail.com>
2022-04-13 22:27:35 -0500

    fixed issues with portfolio page

--- assets/js/loads.js
+++ assets/js/loads.js
@@ -1,13 +1,13 @@
 const loadHTML = (...funcs) => {
-    $('#navbar-container').load('navbar.html', () => {
-        $('#footer-container').load('footer.html', () => {
-            $('#modal-container').load('modals.html', () => {
-                for (let func of funcs) {
-                    func();
-                }
-            })
-        })
-    }) 
+    $('#navbar-container').load('navbar.html');
+    $('#footer-container').load('footer.html');
+    $('#modal-container').load('modals.html');
+    window.addEventListener('load', (event) => {
+        event.preventDefault();
+        for (let func of funcs) {
+            func();
+        }
+    });
 }
 
 export { loadHTML }
